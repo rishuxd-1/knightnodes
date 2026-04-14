157a489b49c43c2fc1c6ae716651bddebd931637
rishuxd-1 <rishabhch106@gmail.com>
2026-04-14 21:49:44 +0530

    Create Pterodactyl configuration file

---
+++ config/pterodactyl.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    api: {
+        endpoint: 'https://api.knightnodes.org',
+        key: 'YOUR_API_KEY_HERE',
+    },
+    domain: {
+        name: 'yourdomain.com',
+        ssl: true,
+    }
+};
